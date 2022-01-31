a76cf4a74697a6c31c6f0f91d15fd6c947e984cf
RakeshKMohanty <mohantykumarrakesh@gmail.com>
2022-01-31 22:31:15 +0530

    Test method written

---
+++ src/test/java/org/practice/EmployeeTest.java
@@ -0,0 +1,17 @@
+package org.practice;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmployeeTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void main() {
+    }
+}
